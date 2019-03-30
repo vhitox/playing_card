d56efbc06b10148e71f3b0f608c30029cd80dfa4
Djoda <victor.paco.flores@gmail.com>
2019-03-30 03:19:39 -0400

    funciones del helper creadas

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:play_cards/utils/card_helper.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   List card_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
+  List<List<int>> matriz = new List.generate(7, (i) => []);
+  CardHelper cHelper = new CardHelper();
+  List c = [0,2,1];
+  List prueba;
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //c = cHelper.orderSelected(c, 2);
+    //print(c);
+    matriz = cHelper.insertData(card_list, matriz);
+    c = cHelper.orderSelected(c, 2);
+    prueba = cHelper.joinMatriz(matriz, c, 2);
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -18,7 +29,7 @@ class MyApp extends StatelessWidget {
             padding: const EdgeInsets.all(20.0),
             crossAxisSpacing: 10.0,
             crossAxisCount: 3,
-            children: _repartir(card_list)            
+            children: _repartir(prueba)            
           ),
         ),
       ),
---
+++ lib/utils/card_helper.dart
@@ -0,0 +1,40 @@
+class CardHelper{
+  static final CardHelper _instance = new CardHelper.internal();
+  factory CardHelper() => _instance;
+  CardHelper.internal();
+  List orderSelected(List list, int s){
+    List past = new List.from(list);
+    List rList = new List(3);
+    rList[1] = s;
+    past.remove(s);
+    //past.shuffle();
+    rList.first = past[0];
+    past.remove(past[0]);
+    rList.last = past[0];
+    print("nuevo valor de guia ${rList}");
+    return rList;
+  }
+  List<List> insertData(List list, List<List> mat){
+    var c = 0;
+    for(var i = 0; i<= 6; i++){
+      for(var j = 0; j<= 2; j++){
+        mat[i].add(list[c]);
+        c++;
+      }
+    }
+    mat.forEach((element) => print(element));
+    return mat;
+  }
+
+  List joinMatriz(List<List> mat, List guia, var select){
+    guia = orderSelected(guia, select);
+    List m = new List();
+    for(var f = 0; f<=2; f++){
+      for(var g = 0; g<=6; g++){
+        m.add(mat[g][guia[f]]);
+      }
+    }
+    print(m);
+    return m;
+  }
+}
