6483553537f4646a02a385e6af2be15b8f7a6424
Djoda <victor.paco.flores@gmail.com>
2019-04-06 18:59:35 -0400

    minimize en lineas enteras 70%

--- lib/main.dart
+++ lib/main.dart
@@ -15,40 +15,38 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Cards extends StatefulWidget {
-  Cards({Key key}) : super(key: key);
+class Cards extends StatefulWidget {  
   @override
   _CardsState createState() => new _CardsState();
 }
 
 class _CardsState extends State<Cards> {
-  final ScrollController _scrollController = ScrollController();
-  List card_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
-  List<List<int>> mat = new List.generate(7, (i) => []);
-  CardHelper hlp = new CardHelper();
+  final ScrollController _scrollController=ScrollController();
+  List crdl=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
+  List<List<int>> mat=new List.generate(7, (i)=>[]);
+  CardHelper hlp=new CardHelper();
   List c;
-  List a_repartir;
-  var veces = 0;
+  List cSet;
+  var t = 0;
   void _selectColumn(int selected) {
     setState(() {
-      veces++;
+      t++;
       c = hlp.orderSelected(c, selected);
-      a_repartir = hlp.joinMatriz(mat, c, selected);
-      mat = hlp.insertData(a_repartir);
+      cSet = hlp.joinMatriz(mat, c, selected);
+      mat = hlp.insertData(cSet);
     });
-    if (veces == 3) {
-      Navigator.push(context,
-          MaterialPageRoute(builder: (context) => YourCard("${mat[3][1]}")));
+    if (t==3) {
+      Navigator.push(context,MaterialPageRoute(builder: (context) => YourCard("${mat[3][1]}")));
     }
   }
 
   @override
   void initState() {
     super.initState();
-    card_list.shuffle();
-    mat = hlp.insertData(card_list);
-    c = [0, 1, 2];
-    a_repartir = card_list;
+    crdl.shuffle();
+    mat = hlp.insertData(crdl);
+    c = [0,1,2];
+    cSet = crdl;
   }
 
   @override
@@ -57,11 +55,7 @@ class _CardsState extends State<Cards> {
         floatingActionButton: Container(
           margin: EdgeInsets.only(right: 14.0),
           width: 300.0,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: _buttons(),
-          )
+          child: Row(mainAxisSize: MainAxisSize.min,mainAxisAlignment: MainAxisAlignment.spaceBetween,children: _buttons())
         ),
         appBar: AppBar(
           title: title,
@@ -75,63 +69,42 @@ class _CardsState extends State<Cards> {
               SliverToBoxAdapter(
                 child: Container(
                   padding: EdgeInsets.only(top: 10.0, left: 20.0),
-                  child: Text(
-                    "Think in a card, and select his column",
-                    style: TextStyle(
-                      color: wh,
-                    ),
-                  ),
-                ),
+                  child: Text("Think in a card, and select his column",style: TextStyle(color: wh)
+                  )
+                )
               ),
               SliverGrid(
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 3,
                 ),
                 delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index) {
-                  print("card${index}");
-                  return Container(
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage(
-                                "assets/images/${a_repartir[index]}.jpg")))
-                  );
-                }, childCount: a_repartir.length)
+                    (BuildContext context, int index) {                  
+                  return Container(decoration: BoxDecoration(image: DecorationImage(image: AssetImage("assets/images/${cSet[index]}.jpg"))));
+                }, childCount: cSet.length)
               )
             ]
           )
         ));
   }
-
   List<Widget> _buttons() {
     List<Widget> retB = List<Widget>();
-    for (var i = 0; i <= 2; i++) {
-      retB.add(FloatingActionButton(
-        heroTag: "${i}",
-        backgroundColor: Colors.black54,
-        child: Icon(Icons.arrow_upward),
-        onPressed: () {
-          _selectColumn(i);
-        },
-      ));
+    for (var i=0;i<=2;i++){
+      retB.add(FloatingActionButton(heroTag: "${i}",backgroundColor: Colors.black54,child: Icon(Icons.arrow_upward),onPressed: () {_selectColumn(i);}));
     }
     return retB;
   }
 }
 
 class YourCard extends StatelessWidget {
-  String yourcard;
-  YourCard(this.yourcard);
+  String ycard;
+  YourCard(this.ycard);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.white54,
-          child: Icon(Icons.arrow_back_ios), onPressed: () {
-            Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => MyApp()));
-          },
+          child: Icon(Icons.arrow_back_ios), onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()));}
         ),
         appBar: AppBar(
           title: title,
@@ -143,18 +116,12 @@ class YourCard extends StatelessWidget {
               children: <Widget>[
                 Container(
                   padding: EdgeInsets.only(top: 20.0, left: 20.0),
-                  child: Text(
-                    "This your card:",
-                    style: TextStyle(color: wh),
-                  )
+                  child: Text("This your card:",style: TextStyle(color: wh))
                 ),
                 Center(
                   child: Container(
                     width: 150.0,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage("assets/images/${yourcard}.jpg"),
-                            fit: BoxFit.fitWidth))
+                    decoration: BoxDecoration(image: DecorationImage(image: AssetImage("assets/images/${ycard}.jpg"),fit: BoxFit.fitWidth))
                   )
                 )
               ]
--- lib/utils/card_helper.dart
+++ lib/utils/card_helper.dart
@@ -1,22 +1,22 @@
 class CardHelper{
-  static final CardHelper _instance = new CardHelper.internal();
-  factory CardHelper() => _instance;
+  static final CardHelper _instance=new CardHelper.internal();
+  factory CardHelper()=>_instance;
   CardHelper.internal();
   List orderSelected(List list, int s){
-    List past = new List.from(list);
-    List rList = new List(3);
-    rList[1] = s;
+    List past=new List.from(list);
+    List rList=new List(3);
+    rList[1]=s;
     past.remove(s);    
-    rList.first = past[0];
+    rList.first=past[0];
     past.remove(past[0]);
-    rList.last = past[0];    
+    rList.last=past[0];    
     return rList;
   }
   List<List> insertData(List list){
-    List<List<int>> mat = new List.generate(7, (i) => []);
-    var c = 0;
-    for(var i = 0; i<= 6; i++){
-      for(var j = 0; j<= 2; j++){
+    List<List<int>> mat=new List.generate(7, (i)=>[]);
+    var c=0;
+    for(var i=0; i<=6; i++){
+      for(var j=0; j<=2; j++){
         mat[i].add(list[c]);
         c++;
       }
@@ -24,10 +24,10 @@ class CardHelper{
     return mat;
   }
   List joinMatriz(List<List> mat, List guia, var select){
-    guia = orderSelected(guia, select);
-    List m = new List();
-    for(var f = 0; f<=2; f++){
-      for(var g = 0; g<=6; g++){
+    guia=orderSelected(guia, select);
+    List m=new List();
+    for(var f=0; f<=2; f++){
+      for(var g=0; g<=6; g++){
         m.add(mat[g][guia[f]]);
       }
     }    
